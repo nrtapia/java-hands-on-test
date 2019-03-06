fe2c0310cc5513ec593596b1b9ea5db4777ae068
Neider Tapia Avila <neider.tapia@boomi.com>
2019-03-05 21:55:36 -0500

    Add Angular component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchFormComponent } from './employee/search-form/search-form.component';
+import { DataListComponent } from './employee/data-list/data-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchFormComponent,
+    DataListComponent
   ],
   imports: [
     BrowserModule,
